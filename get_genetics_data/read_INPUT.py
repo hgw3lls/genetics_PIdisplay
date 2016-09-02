ebb05621c8d048122ed2cbb4ce18541dcfadf26c
tyanick <tony.yanick@thenewcentre.org>
2016-09-02 14:34:27 -0400

    Added image creation

---
+++ get_genetics_data/read_INPUT.py
@@ -0,0 +1,88 @@
+import csv
+import urllib
+import PIL
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+import textwrap
+
+urllib.urlretrieve("http://genetics.case.edu/VidDISPLAY/INPUT", "INPUT")
+
+allinfo=[]
+
+
+with open('INPUT','r') as f:
+    # skip headings
+    # next(f) 
+    reader=csv.reader(f,delimiter='\t')
+    for info in reader:
+        allinfo.append(info)
+
+talktype_font = ImageFont.truetype("../fonts/TitilliumMaps26L.otf",175)
+speaker_font = ImageFont.truetype("../fonts/TitilliumMaps26L001.otf",150)
+title_font = ImageFont.truetype("../fonts/TitilliumMaps26L002.otf",100)
+description_font = ImageFont.truetype("../fonts/TitilliumTitle12.otf",100)
+description_font = ImageFont.truetype("../fonts/TitilliumTitle12.otf",100)
+day_time_font = ImageFont.truetype("../fonts/Titalic750wt.otf",100)
+loc_font = ImageFont.truetype("../fonts/TitilliumMaps26L003.otf",100)
+check_event=[]
+count = 0
+margin = 45
+offset= 665
+for event in allinfo:
+	Type = event[0]
+	Number = event[1]
+	Background_Img = event[2]
+
+	img=Image.open(Background_Img)
+	draw = ImageDraw.Draw(img)
+
+	if Type == "REG": #3-11
+		Number = event[5]
+		Dayoftheweek = event[6]
+		TalkType = event[7]
+		Speaker = event[8]
+		Title = event[9]
+		Time =  event[10]
+		Location = event[11]
+		Day_Time = Dayoftheweek + ', ' + Time
+
+		
+
+		
+		draw.text((1000, 200),TalkType,(0,0,0),font=talktype_font)
+		draw = ImageDraw.Draw(img)
+		draw.text((1000, 490),Speaker,(0,0,0),font=speaker_font)
+		draw = ImageDraw.Draw(img)
+		for line in textwrap.wrap(Title, width=45):
+    			draw.text((1000, offset), line, (0,0,0), font=title_font)
+    			offset += title_font.getsize(line)[1]
+    			
+
+
+		#draw.text((1000, 550),Title,(0,0,0),font=title_font)
+		draw = ImageDraw.Draw(img)
+		draw.text((1000, 1250),Day_Time,(0,0,0),font=day_time_font)
+		draw = ImageDraw.Draw(img)
+		draw = ImageDraw.Draw(img)
+		draw.text((1000, 1350),Location,(0,0,0),font=loc_font)
+		draw = ImageDraw.Draw(img)
+		image_name = "slide" + str(count) +".png"
+		img.save(image_name)
+		count = count+1
+	elif Type == "NEWS": #3-6
+		print event[3]
+		print event[4]
+		print event[5]
+	elif Type == "SPEC":
+		print "yes"
+	else: 
+		print "Bad Type: I don't have this type in my logic."
+	
+	
+
+
+
+
+
+
